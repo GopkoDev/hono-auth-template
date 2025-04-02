590f843c5371a6a567903b520673af15388b856d
Dmytro Hopko <gopkodimon@gmail.com>
2025-04-02 21:12:14 +0000

    use env for port

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import 'dotenv/config';
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+const port = parseInt(process.env.PORT || '4200', 10);
+serve(
+  {
+    fetch: app.fetch,
+    port: port,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
